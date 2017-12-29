88818f867270a6e2ad32287169aadaf9cdfc6a0f
chenghongyan <chenghy.js@eakay.cn>
2017-12-29 13:42:19 +0800

    审核平台

---
+++ src/main/java/com/springmvc/controller/AuditProcessControl.java
@@ -0,0 +1,17 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+* @Title: AuditProcessControl
+* @Description:
+* @author chy
+* @date 2017/12/29 11:42
+*/
+@Controller
+@RequestMapping("/auditProcess")
+public class AuditProcessControl {
+
+
+}
---
+++ src/main/java/com/springmvc/controller/AuditStepControl.java
@@ -0,0 +1,16 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+* @Title: AuditStepControl
+* @Description:
+* @author chy
+* @date 2017/12/29 11:43
+*/
+@Controller
+@RequestMapping("/auditStep")
+public class AuditStepControl {
+
+}
